14b901506f16264890792d1f3f84487078d9d5f4
Raphael Cohn <raphael.cohn@stormmq.com>
2018-08-16 11:20:16 +0100

    Minor tweak

--- workspace/dpdk/src/EthernetPort.rs
+++ workspace/dpdk/src/EthernetPort.rs
@@ -5,7 +5,6 @@
 /// An ethernet port (link).
 pub trait EthernetPort
 {
-	// ETH_RSS_L2_PAYLOAD is rarely supported and ETH_RSS_PORT is almost unused.
 	const DesiredReceiveSideScalingHashProtocols: u64 = ETH_RSS_IP | ETH_RSS_UDP | ETH_RSS_TCP;
 	
 	/// After `rte_eal_init()` is called, it is safe to call this function.
@@ -15,7 +14,7 @@ pub trait EthernetPort
 	/// When this function returns with a `Some()`, then the link is fully configured and up. It should be freed by calling `self.terminate_started_device_and_hot_plug_detach()`.
 	///
 	/// If it returns with `None`, then the calling process should terminate.
-	fn configure_and_start<Handler: EthernetPortLinkStatusEventHandler>(self, toeplitz_hash_function_key_data_strategy: &ToeplitzHashFunctionKeyDataStrategy, handler: Handler, should_function_terminate: &Arc<ShouldFunctionTerminate>, media_access_control_address: MediaAccessControlAddress) -> Option<Box<Handler>>
+	fn configure_and_start<Handler: EthernetPortLinkStatusEventHandler>(self, hash_function_key_data_strategy: &HashFunctionKeyDataStrategy, handler: Handler, should_function_terminate: &Arc<ShouldFunctionTerminate>, media_access_control_address: MediaAccessControlAddress) -> Option<Box<Handler>>
 	{
 		/*
 			Cap on rss queues - size of RETA table (64, 128 or 512 entries)
