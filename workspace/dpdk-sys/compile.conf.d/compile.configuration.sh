165f32a0e9eba62db0469b0348edf5f1834a0664
Raphael Cohn <raphael.cohn@stormmq.com>
2018-08-17 17:40:32 +0100

    Copyright fix

--- workspace/dpdk-sys/compile.conf.d/compile.configuration.sh
+++ workspace/dpdk-sys/compile.conf.d/compile.configuration.sh
@@ -1,5 +1,5 @@
-# This file is part of ucx-sys. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/ucx-sys/master/COPYRIGHT. No part of ucx-sys, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
-# Copyright © 2016 The developers of ucx-sys. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/ucx-sys/master/COPYRIGHT.
+# This file is part of dpdk. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk/master/COPYRIGHT. No part of dpdk, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+# Copyright © 2016 The developers of dpdk. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk/master/COPYRIGHT.
 
 
 compile_library_name='dpdk'
